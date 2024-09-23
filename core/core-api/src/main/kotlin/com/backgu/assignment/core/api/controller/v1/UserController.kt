92f28bfc7b383d17a5e46af46cd1ee891110f0d3
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-09-23 11:19:07 +0900

    MDC 설정 추가

--- core/core-api/src/main/kotlin/com/backgu/assignment/core/api/controller/v1/UserController.kt
+++ core/core-api/src/main/kotlin/com/backgu/assignment/core/api/controller/v1/UserController.kt
@@ -5,11 +5,14 @@ import com.backgu.assignment.core.api.controller.v1.request.SignUpRequest
 import com.backgu.assignment.core.api.controller.v1.response.SignUpResponse
 import com.backgu.assignment.core.api.support.response.ApiResponse
 import com.backgu.assignment.core.domain.user.UserService
+import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+
+private val logger = KotlinLogging.logger {}
 
 @RestController
 @RequestMapping("/api/v1")
@@ -26,10 +29,12 @@ class UserController(
         @RequestBody request: LoginRequest,
         httpServletRequest: HttpServletRequest,
     ): ApiResponse<Unit> {
+        logger.info { "login request: $request" }
         val sessionId = userService.login(request.toLoginUser())
 
         httpServletRequest.session.setAttribute("AUTH", sessionId)
 
+        logger.info { "login success: $sessionId" }
         return ApiResponse.success(Unit)
     }
 }
--- support/logging/build.gradle.kts
+++ support/logging/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
+    api("io.github.oshai:kotlin-logging-jvm:7.0.0")
     implementation("io.micrometer:micrometer-tracing-bridge-brave")
     implementation("io.sentry:sentry-logback:${property("sentryVersion")}")
 }
